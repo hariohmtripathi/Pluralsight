8b2803aa974415b083557bc7c80280b7942abf6a
Sanjiv358 <sanjivmahto8@gmail.com>
2022-07-24 01:42:39 +0530

    footer

---
+++ footer/footer.js
@@ -0,0 +1,81 @@
+function footerr(){
+    return ` <div id="footer">
+    <div>
+        <h3>SOLUTIONS</h3>
+        <p>Pluralsight Skills</p>
+        <p>Pluralsight Flow</p>
+        <p>Government</p>
+        <p>Gift of Pluralsight</p>
+        <p>View Pricing</p>
+        <p>Contact Sales</p>
+        <p>Skill up for free</p>
+    </div>
+    <div>
+        <h3>PLATFORM</h3>
+        <p>Browse Library</p>
+        <p>Role IQ</p>
+        <p>Skill IQ</p>
+        <p>Iris</p>
+        <p>Authors</p>
+        <p>Professional Services</p>
+    </div>
+    <div>
+        <h3>COMPANY</h3>
+        <p>About us</p>
+        <p>Customer stories</p>
+        <p>Careers</p>
+        <p>Blog</p>
+        <p>Newsroom</p>
+        <p>Resource center</p>
+        <p>Guides</p>
+    </div>
+    <div>
+        <h3>RESOURCES</h3>
+        <p>Download Pluralsight</p>
+        <p>Events</p>
+        <p>Teach</p>
+        <p>Partners</p>
+        <p>Affilate Partners</p>
+        <p>PluralsightOne.org</p>
+        <p>Subscribe</p>
+    </div>
+    <div>
+        <h3>SUPPORT</h3>
+        <p>Contact</p>
+        <p>Help center</p>
+        <p>IP allowlist</p>
+        <p>Sitemap</p>
+    </div>
+</div>
+<hr>
+<div id="lan">
+    <img src="https://www.pluralsight.com/content/dam/pluralsight2/icons/globe-white.png" alt="">
+    <p>Deutsch</p>
+    <p style="color: white;">English</p>
+    <p>French</p>
+</div>
+<hr>
+<div id="cright">
+    <div class="logo">
+        <img src="https://www.pluralsight.com/content/dam/pluralsight2/general/icons/light/ps-icon.png" alt="">
+        <p>Copyright © 2004 - 2022 Pluralsight LLC. All rights reserved.</p>
+    </div>
+    <div class="icon">
+        <h4 ><a href="https://www.pluralsight.com/terms">Terms of Use</a></h4>
+        <h4><a href="https://www.pluralsight.com/privacy">Privacy Notice</a></h4>
+        <h4>Modern Slavery Act Transparency Statement</h4>
+        
+        
+        <a href="https://www.facebook.com/pluralsight"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        <a href="https://www.instagram.com/pluralsight/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        <a href="https://twitter.com/pluralsight"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+        <a href="https://www.linkedin.com/company/pluralsight/"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+        <a href="https://www.youtube.com/pluralsight"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+    
+    </div>
+</div>`
+}
+
+
+
+export default footerr
